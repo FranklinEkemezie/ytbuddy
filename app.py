320901448381cd4c7e11544d40cf8ac51056c538
FranklinEkemezie <franklynpeter2006@gmail.com>
2024-10-26 14:59:46 +0100

    add index page to codebase

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Welcome to the YouTube Downloader!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
